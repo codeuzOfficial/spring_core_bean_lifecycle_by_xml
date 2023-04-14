61cb71722ce208a18f61d661cc5ec4bb208f2802
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2023-04-14 14:58:54 +0500

    Man class changed

--- src/main/java/dasturlash/uz/Main.java
+++ src/main/java/dasturlash/uz/Main.java
@@ -1,13 +1,14 @@
 package dasturlash.uz;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         Student student = (Student) context.getBean("student");
 
         System.out.println(student);
+        context.registerShutdownHook();
     }
 }
